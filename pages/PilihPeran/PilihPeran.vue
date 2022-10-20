9bb3678dac8f0249514c036dd711edb5d9091e25
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2022-10-20 19:03:53 +0700

    add halaman pilih peran

---
+++ pages/PilihPeran/PilihPeran.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper">
+    <h3>Pilih Peran</h3>
+    <div class="guru">
+      <img src="~/assets/img/pilihPeran/peranGuru.png" alt="">
+    </div>
+    <div class="siswa">
+      <img src="~/assets/img/pilihPeran/peranSiswa.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+    .h3{
+        font-size: 2.5em;
+    }
+
+    .guru {
+        width: 126px;
+        height: 126px;
+        background:  #FFD481;
+        border-radius: 19.6875px;
+    }
+
+    .siswa {
+        width: 126px;
+        height: 126px;
+        background:#FFF3B6 ;
+        border-radius: 19.6875px;
+    }
+
+    .wrapper div{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+    }
+
+    .wrapper{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 100vh;
+        gap: 32.82px;
+        overflow:visible;
+    }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <Tutorial />
+  <div class="root">
+    <PilihPeran />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import PilihPeran from './PilihPeran/PilihPeran.vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { PilihPeran }
 })
 </script>
+
+<style>
+  body{
+    margin: 0px;
+    padding: 0px;
+    background-color: blue;
+  }
+
+  *{
+    box-sizing: border-box;
+  }
+</style>
