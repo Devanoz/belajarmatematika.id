f5755e1086aca7740074d4a7fc4a3d9afd1adc5b
yuanda@Lab32 <34027873+yuandahanif@users.noreply.github.com>
2022-10-25 17:34:00 +0700

    feat: :sparkles: email verification page

---
+++ pages/auth/email-verification.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col h-full pt-10 text-center">
+    <h1 class="text-5xl text-cyan-700 mb-8">
+      Verifikasi E-mail
+    </h1>
+
+    <span class="text-base font-light text-slate-400">Tautan verifikasi telah di kirim ke email
+      <span class="font-bold">email@email.com</span>.
+      Cek Inbox atau folder spam kamu ya.</span>
+
+    <span>Ingin ganti email? <a href="#">Ubah email</a></span>
+
+    <!-- gambar rumah -->
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'EmailVerificationPage'
+})
+</script>
