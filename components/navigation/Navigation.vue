0abe20286a973a8beb450573ce775386a85f552d
ihsan606 <ihsansyafiul@gmail.com>
2022-11-03 17:11:01 +0700

    edit navigation

--- components/navigation/Navigation.vue
+++ components/navigation/Navigation.vue
@@ -1,52 +1,96 @@
 <template>
-  <div class="bg-white rounded-t-xl shadow-md px-10 sm:px-20 py-6 max-h-[6rem] max-w-screen-sm fixed bottom-0 mx-auto w-full border border-slate-200">
-    <div
-      class="h-full flex justify-between items-center w-full"
-    >
-      <NuxtLink to="/materi">
-        <div class="flex flex-col items-center gap-y-1">
-          <img class="h-6 w-auto" src="~/assets/img/navbar/basic/materi.svg">
-          <span class="text-slate-400">Materi</span>
-        </div>
-      </NuxtLink>
+<div class="bg-white rounded-t-xl shadow-md px-10 sm:px-20 py-6 max-h-[6rem] max-w-screen-sm fixed bottom-0 mx-auto w-full border border-slate-200">
+    <div class="h-full flex justify-between items-center w-full">
+        <button @click="onMateriClicked">
 
-      <NuxtLink to="/video">
-        <div class="flex flex-col items-center gap-y-1">
-          <img class="h-6 w-auto" src="~/assets/img/navbar/basic/video.svg">
-          <span class="text-slate-400">Video</span>
-        </div>
-      </NuxtLink>
+            <div class="flex flex-col items-center gap-y-1">
+                <img v-if="materiClicked" class="h-6 w-auto" src="~/assets/img/navbar/basic/materi-color.svg">
+                <img v-else class="h-6 w-auto" src="~/assets/img/navbar/basic/materi.svg">
+                <span class="text-slate-400">Materi</span>
+            </div>
+        </button>
 
-      <div class="flex flex-col items-center gap-y-1">
-        <img class="h-6 w-auto" src="~/assets/img/navbar/basic/tantangan.svg">
-        <span class="text-slate-400">Tantangan</span>
-      </div>
+        <button @click="onVideoClicked">
+            <div class="flex flex-col items-center gap-y-1">
+                <img v-if="videoClicked" class="h-6 w-auto" src="~/assets/img/navbar/basic/video-color.svg">
+                <img v-else class="h-6 w-auto" src="~/assets/img/navbar/basic/video.svg">
+                <span class="text-slate-400">Video</span>
+            </div>
+        </button>
 
-      <NuxtLink to="/leaderboard">
-        <div class="flex flex-col items-center gap-y-1">
-          <img class="h-6 w-auto" src="~/assets/img/navbar/basic/peringkat.svg">
-          <span class="text-slate-400">Peringkat</span>
+        <div @click="onTantanganClicked" class="flex flex-col items-center gap-y-1">
+            <img v-if="tantanganClicked" class="h-6 w-auto" src="~/assets/img/navbar/basic/tantangan-color.svg">
+            <img v-else class="h-6 w-auto" src="~/assets/img/navbar/basic/tantangan.svg">
+            <span class="text-slate-400">Tantangan</span>
         </div>
-      </NuxtLink>
+
+        <button @click="onPeringkatClicked">
+            <div  class="flex flex-col items-center gap-y-1">
+                <img v-if="peringkatClicked" class="h-6 w-auto" src="~/assets/img/navbar/basic/peringkat-color.svg">
+                <img v-else class="h-6 w-auto" src="~/assets/img/navbar/basic/peringkat.svg">
+                <span class="text-slate-400">Peringkat</span>
+            </div>
+        </button>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      clicked: false
-    }
-  },
-  methods: {
-    doClick () {
-      this.clicked = !this.clicked
+    data() {
+        return {
+            materiClicked: true,
+            videoClicked: false,
+            tantanganClicked: false,
+            peringkatClicked: false
+        }
     },
-    getColor () {
-      return (this.clicked) ? 'red' : ''
+    mounted() {
+      
+    },
+    methods: {
+        onMateriClicked() {
+            this.$router.push({
+                name: 'materi',
+            })
+
+            this.materiClicked = !this.materiClicked
+            this.videoClicked = false
+            this.tantanganClicked = false
+            this.peringkatClicked = false
+        },
+
+        onVideoClicked() {
+            this.$router.push({
+                name: 'video',
+            })
+            this.materiClicked = false
+            this.videoClicked = true
+            this.tantanganClicked = false
+            this.peringkatClicked = false
+        },
+
+        onTantanganClicked() {
+            this.$router.push({
+                name: 'tantangan',
+            })
+            this.materiClicked = false
+            this.videoClicked = false
+            this.tantanganClicked = true
+            this.peringkatClicked = false
+        },
+
+        onPeringkatClicked() {
+            this.$router.push({
+                name: 'leaderboard',
+            })
+            this.materiClicked = false
+            this.videoClicked = false
+            this.tantanganClicked = false
+            this.peringkatClicked = true
+        },
+
     }
-  }
 }
 </script>
 
---
+++ pages/tantangan/index.vue
@@ -0,0 +1,13 @@
+<template >
+    <div class="flex flex-col h-full px-4 py-4">
+        
+    </div>
+</template>
+<script>
+export default {
+    layout:"app"
+}
+</script>
+<style lang="ts">
+    
+</style>
