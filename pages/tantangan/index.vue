f23c0655b7591e1e19c06f8d0df893e62053be56
ihsan606 <ihsansyafiul@gmail.com>
2022-11-07 10:19:45 +0700

    add tantangan history

--- pages/tantangan/index.vue
+++ pages/tantangan/index.vue
@@ -5,8 +5,11 @@
                 <span class="text-2xl text-gray-700 font-semibold col-start-1 col-span-3">Ayo Bermain</span>
                 <span class=" text-lg text-gray-400 font-normal">Sambil belajar!</span>
             </div>
-            <img class="text-3xl rounded-full justify-end col-end-4 place-self-end self-start"
+            <nuxt-link to="/tantangan/history" class="justify-end col-end-4 place-self-end self-start">
+                <img class="text-3xl rounded-full  h-7 w-7"
                 src="@/assets/img/tantangan/history.svg" alt="">
+            </nuxt-link>
+            
         </div>
 
         <!-- ongoing -->
