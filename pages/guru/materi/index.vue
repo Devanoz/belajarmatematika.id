1cd713d2a8db92f1063c833cb3c183c6da5327f3
ihsan606 <ihsansyafiul@gmail.com>
2022-12-01 12:28:56 +0700

    integrasi login guru

--- pages/guru/materi/index.vue
+++ pages/guru/materi/index.vue
@@ -27,31 +27,9 @@
         <!-- dropdown kelas -->
 
         <div class="relative inline-block text-left col-span-4 mr-2 z-20">
-            <div>
-                <button @click="onDropdownClassClicked" type="button" class="inline-flex w-full justify-center rounded-2xl border border-gray-300 bg-sky-100 px-4 py-1 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 focus:ring-offset-sky-100" id="menu-button" aria-expanded="true" aria-haspopup="true">
-                    Kelas
-                    <!-- Heroicon name: mini/chevron-down -->
-                    <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                    </svg>
-                </button>
-            </div>
-
-            <div class="absolute z-auto mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" style="z-index: 999;" :class="{ 'hidden': dropdownClassClicked == false }" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                <div class="py-1" role="none">
-                    <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Kelas 4</a>
-                </div>
-                <div class="py-1" role="none">
-                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Kelas 5</a>
-
-                </div>
-                <div class="py-1" role="none">
-                    <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-4">Kelas 6</a>
-
-                </div>
-
-            </div>
+            <multiselect v-model="filter.kelas" :options="provinces" :searchable="true" placeholder="Select one" selected-label="" select-label="" deselect-label="">
+               
+            </multiselect>
         </div>
 
         <!-- <div class="col-span-1"></div> -->
@@ -142,10 +120,18 @@ import {
     topiks
 } from "@/static/topiks";
 
+import Multiselect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
+
 export default {
     layout: 'guru',
     data() {
         return {
+            filter: {
+                kelas: '',
+                materi: ''
+            },
+            kelas: [],
             topiks: topiks,
             materis: materi,
             dropdownClassClicked: false,
